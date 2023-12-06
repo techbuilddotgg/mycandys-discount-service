3e89c04263b4cdb7d34ab5f3d9552206f227e9cd
gregorsulcer <workcontroller0@gmail.com>
2023-12-06 22:46:58 +0100

    fix: env settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,23 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { DiscountsModule } from './api/discounts/discounts.module';
 import { OffersModule } from './api/offers/offers.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URI, {
-      dbName: process.env.DB_NAME,
+    ConfigModule.forRoot({
+      envFilePath: `${process.cwd()}/.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+      load: [configuration],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+        dbName: configService.get<string>('DB_NAME'),
+      }),
+      inject: [ConfigService],
     }),
     DiscountsModule,
     OffersModule,
